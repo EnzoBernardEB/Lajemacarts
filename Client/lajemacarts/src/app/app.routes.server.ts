48145b6ec08241897cbb1d3bfc106715d8f95046
EnzoBernardEB <68954967+EnzoBernardEB@users.noreply.github.com>
2025-07-10 16:14:10 +0200

    Remove placeholder code in `app.component.html`

--- Client/lajemacarts/src/app/app.routes.server.ts
+++ Client/lajemacarts/src/app/app.routes.server.ts
@@ -1,7 +1,12 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
+import {RenderMode, ServerRoute} from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'dashboard',
+    renderMode: RenderMode.Client,
+ },
+
+{
     path: '**',
     renderMode: RenderMode.Prerender
   }
